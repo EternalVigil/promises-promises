cee536543db72146bb8c50a19b01fbb978942e33
EternalVigil <theeternalvigil@gmail.com>
2022-08-03 11:04:29 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { PromiseContainer } from './PromiseContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PromiseContainer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PromiseContainer.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const url = 'https://www.boredapi.com/api/activity';
+
+export const PromiseContainer = () => {
+    const [fetchedData, setFetchedData] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const vanillaGet = () => {
+        return new Promise((resolve, reject) => {
+            let request = new XMLHttpRequest();
+            request.open('GET', url);
+            setLoading(true);
+
+            request.onload = () => {
+                if(request.status === 200) {
+                    resolve(request.response);
+                    setLoading(false);
+                } else {
+                    setLoading(false);
+                    reject(Error(request?.statusText));
+                }
+            };
+
+            request.send();
+        });
+    };
+
+    const handleVanillaGet = async () => {
+        await vanillaGet().then((response) => {
+            return JSON.parse(response);
+        }).then((data) => {
+            setFetchedData(data);
+        });
+    };
+
+    const axiosGet = async () => {
+        setLoading(true);
+        return await axios.get(url)
+            .then((response) => {
+                setFetchedData(response?.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log('error', error);
+                setLoading(false);
+            });
+    };
+
+    const handleClearData = () => {
+        setFetchedData(null);
+    };
+
+    return(
+        <div>
+            Promises go here
+            <div>
+                <button onClick={handleVanillaGet}>
+                    Vanilla Promise
+                </button>
+            </div>
+            <div>
+                <button onClick={axiosGet}>
+                    Axios Promise
+                </button>
+            </div>
+            <div>
+                <button onClick={handleClearData}>
+                    Clear Data
+                </button>
+            </div>
+            <div>
+                <div>
+                    Status: {loading ? 'request in flight' : 'inactive'}
+                </div>
+                <div>
+                    Received Data
+                </div>
+                <div>
+                    Activity: {fetchedData?.activity}
+                </div>
+            </div>
+        </div>
+    );
+};
